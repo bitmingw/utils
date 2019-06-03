a5afc778fc87ca92a77b548cd234d605c388676a
Ming Wen <bitmingw@gmail.com>
2019-06-03 14:50:57 -0700

    use spaceship theme

--- zsh-config/INSTALL.sh
+++ zsh-config/INSTALL.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# Install Oh My Zsh
 echo "Configuring zsh..."
 sleep 1
-
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "Downloading spaceship theme..."
+sleep 1
+if [ -z "$ZSH_CUSTOM" ]; then
+    export ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+fi
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 echo "Setup .zshrc file..."
 sleep 1
-cp .zshrc $HOME
+cp .zshrc "$HOME"
